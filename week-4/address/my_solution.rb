768dee54ac30b0901d97d02e0147fcce963f2533
Luis De Castro <HolixSF@users.noreply.github.com>
2015-11-12 17:58:37 -0800

    Update my_solution.rb

--- week-4/address/my_solution.rb
+++ week-4/address/my_solution.rb
@@ -1,12 +1,17 @@
 # Format an Address
 
-def make_address(street, city, state, zip)
-	p "You live at " + street + ", in the beautiful city of " + city + ", " + state + "." + " Your zip is " + zip.to_s + "."
-end
+#create method that takes in 4 parameters
+#return sentence using 4 parameters for street, city, state, and zip
+#turn interger into string
 
-make_address("633 Folsom St.", "San Francisco", "CA", "94107")
 
 # I worked on this challenge [by myself, with: ].
 
 
 # Your Solution Below
+
+def make_address(street, city, state, zip)
+	p "You live at " + street + ", in the beautiful city of " + city + ", " + state + "." + " Your zip is " + zip.to_s + "."
+end
+
+make_address("633 Folsom St.", "San Francisco", "CA", "94107")
