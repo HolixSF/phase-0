5208ffc182c784d2b2076e7d74b0d04a914c3843
Luis De Castro <HolixSF@users.noreply.github.com>
2015-11-12 18:04:20 -0800

    Update my_solution.rb

--- week-4/leap-years/my_solution.rb
+++ week-4/leap-years/my_solution.rb
@@ -6,20 +6,14 @@
 # Your Solution Below
 
 def leap_year?(year)
-	if year % 4 == 0 			# WILL RETURN TRUE IF DIVISIBLE BY 4
-								# 
-    	if year % 100 == 0 		# IF IT IS DIVISIBLE BY 100 WILL RETURN FALSE
-
-        	if year % 400 == 0 	# ONLY IF IT IS DIVISIBLE BY 400 WILL IT RETURN TRUE
-        						# SO IF IT IS NOT DIVISIBLE BY 400 IT WILL RETURN FALSE
-        						# MAKING THE ARGUMENT OF 100 ABOVE TRUE THEREFORE 4 AND 100 IS TRUE WHILE 400 IS FALSE
-            	return true
-         	end
-
-      	return false
-      	end
-
-    return true
+	if year % 4 == 0 			# WILL RETURN TRUE IF DIVISIBLE BY 4			
+    		if year % 100 == 0 		# IF IT IS DIVISIBLE BY 100 WILL RETURN FALSE
+    			if year % 400 == 0 	# ONLY IF IT IS DIVISIBLE BY 400 WILL IT RETURN TRUE
+            		return true		# SO IF IT IS NOT DIVISIBLE BY 400 IT WILL RETURN FALSE
+         		end			# MAKING THE ARGUMENT OF 100 ABOVE TRUE THEREFORE 4 AND 100 IS TRUE WHILE 400 IS FALSE
+         	return false
+      		end
+      	return true
 
    	else
    		return false 			#EVERYTHING ELSE IS FALSE
