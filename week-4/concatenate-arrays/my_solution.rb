10e140fe1e6e06b746c44142f9ffb89be2e769c8
Luis De Castro <luisdecastro510@gmail.com>
2015-11-13 16:00:11 -0800

    Add solo-challenge

---
+++ week-4/concatenate-arrays/my_solution.rb
@@ -0,0 +1,19 @@
+# Concatenate Two Arrays
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def array_concat(array_1, array_2)
+  # Your code here
+
+  #(First Solution)
+
+  #new_array = array_1 + array_2
+  #return new_array
+
+  #(Second Solution)
+  
+  array_1.concat(array_2)
+end
